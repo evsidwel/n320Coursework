88a0c63dc038357a67b510f50a623e426864ec4c
zondervoze <zondervoze@gmail.com>
2019-09-24 13:54:10 -0400

    submission

---
+++ labs/Week3/js/app.js
@@ -0,0 +1,62 @@
+class Ball {
+  //Creates a class "Ball". The ball uses the module pattern. Allows multiple instances of this ball class.
+
+  constructor() {
+    // The Constructor.
+    // Defines the position attribute.
+    this.position = { x: 100, y: 100 };
+    // Defines the velocity attribute.
+    this.velocity = { x: 10, y: 0 };
+  }
+
+  update() {
+    // Defines what Ball does during the P5 draw method.
+
+    // Changes the x position by adding the x velocity.
+    this.position.x += this.velocity.x;
+    // Changes the y postion by adding the y velocity.
+    this.position.y += this.velocity.y;
+
+    // Draws a circle at the x and y position of size 20.
+    circle(this.position.x, this.position.y, 20);
+
+    // If the ball is beyon the x upper and lower bounds: call the ballBeyond method and passes the current instance of ball to it.
+    if (this.position.x < 0 || this.position.x > 400) {
+      World.ballBeyond(this);
+    }
+  }
+}
+
+var World = {
+  // Defines the canvas for the p5 application. This uses a mediator pattern where other objects interact.
+
+  // Sets the background color in rbg.
+  bgcolor: [237, 119, 83],
+
+  // Defines the ballBeyond method which accepts a ball input.
+  ballBeyond: function(whichBall) {
+    // Changes the background color when this funtion is called.
+    this.bgcolor = [Math.random() * 255, Math.random() * 255, 83];
+    // Resets the passed ball's x position
+    whichBall.position.x = 100;
+    // Resets the passed ball's x velocity
+    whichBall.velocity.x = (Math.random() - 0.5) * 20;
+  }
+};
+
+//class for a box
+//Grows in size every time a ball hits an edge and is reset
+// "For fun": multiple balls
+
+var ball = new Ball(); // Create a new instance of the Ball class called ball
+
+function setup() {
+  // P5 application essential
+  createCanvas(400, 300);
+}
+
+function draw() {
+  // P5 application essential
+  background(World.bgcolor);
+  ball.update();
+}
